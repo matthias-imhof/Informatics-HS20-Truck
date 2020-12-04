a2ebe3a9fc1f0f8c530779694498524ec93fa720
Matthias Imhof <matthias.imhof@imhof-lan.ch>
2020-12-04 16:26:51 +0100

    Added Abstractmethod and more options for packages and also made the delivery possible for the trucks

--- truck/Truck.py
+++ truck/Truck.py
@@ -1,15 +1,31 @@
 from abc import ABC, abstractmethod
 
-class Package:
-	def __init__(self, name, weight, distance):
+class Package(ABC):
+	def __init__(self, name, weight, distance, sender):
 		self._name = name
 		self._weight = weight
 		self._distance = distance
+		self._owner = sender
 		
 	def __repr__(self):
 		return '{}'.format((self._name, self._weight, self._distance))
-		
+	@abstractmethod
+	def costs(self):
+		pass
 
+class Parcel(Package):
+	def costs(self):
+		self._costs = self._weight * (self._distance / 100)
+		# apply discount for large objects
+		if self._costs >= 100000:
+			self._costs = 100000
+		return self._costs
+
+class Letter(Package):
+	def costs(self):
+		self._costs = self._distance / 100
+		return self._costs
+	
 class TruckingCompany:
 	def __init__(self, trucks, parcel):
 		self._trucks = trucks
@@ -40,23 +56,36 @@ class Truck:
 	
 	def current_cargo(self):
 		return self._cargo[:]
+	
+	def deliver(self):
+		if len(self._cargo) > 0:
+			max = self._cargo[0]._distance
+		else:
+			return 'Nothing to deliver'
+		for pr in self._cargo:
+			if pr._distance >= max:
+				max = pr._distance
+		self._range -= max
 
 if __name__ == '__main__':
 	veh = [
-		Truck(1000,1000, 'Base')
+		Truck(1000,1000, 'Base'),
+		Truck(100000,10000, 'Washington')
 	]
-	parcel = [
-		Package('Exams',100,40),
-		Package('Gandalf Weed', 300,100),
-		Package('Gandalf Weed', 300,100),
-		Package('Gandalf Weed', 300,100),
-		Package('Gandalf Weed', 300,100),
-		Package('Gandalf Weed', 300,100),
-		Package('Haggrid and friends',10000,100000)
+	packages = [
+		Letter('Exams',100,40, 'Prof. Dr. Python'),
+		Parcel('Gandalf Weed', 300,100, 'Gandalf'),
+		Parcel('Gandalf Weed', 300,100, 'Gandalf'),
+		Parcel('Gandalf Weed', 300,100, 'Gandalf'),
+		Parcel('Gandalf Weed', 300,100, 'Gandalf'),
+		Parcel('Gandalf Weed', 300,100, 'Gandalf'),
+		Parcel('Haggrid and friends',10000,100000, 'Haggrid')
 	]
 	
-	t = TruckingCompany(veh, parcel)
+	t = TruckingCompany(veh, packages)
 	print(t.load_parcel_to_truck())
 	print(veh[0].current_cargo())
 	print(veh[0]._cur_weight)
-		
+	
+	veh[0].deliver()
+	print(veh[0]._range)
